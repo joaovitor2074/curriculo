5f88a45b49117915be3f893dea87ffdfd8c96390
Joao vitor <jvzinn2074@gmail.com>
2025-09-19 19:25:31 -0300

    algo

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/curriculo/', // 👈 importante para GitHub Pages
+  base: '/curriculo/', // 👈 necessário para GitHub Pages
 })
